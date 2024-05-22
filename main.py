f39854cbbaf1de0560772b5c415ba6d5457f2e65
Ran4er <danileh.hromov@gmail.com>
2024-05-22 19:17:41 +0300

    Completed Laboratory No. 10

---
+++ main.py
@@ -0,0 +1,28 @@
+import exercise7 as seven
+import exercise8 as eight
+import exercise9 as nine
+
+info = '''Напишите число, которое соответствует заданию из списка:
+\t1. Упражнение 7
+\t2. Упражнение 8
+\t3. Упражнение 9
+\t0. Выход'''
+while True:
+     print(info)
+     a = input('Пожалуйста введите число: ')
+     match a:
+         case '1':
+             seven.print_solution()
+             print("\n\n")
+         case '2':
+             eight.print_solution()
+             print("\n\n")
+         case '3':
+             nine.print_solution()
+             print("\n\n")
+         case '0':
+             print('Bye-bye :(')
+             break
+         case _:
+             print("Вы ввели некорректное значение\nПожалуйста введите число из списка")
+             print("\n\n")
